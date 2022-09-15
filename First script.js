bea69259bff3924829052c14b90e6420b0328570
BlossomEze <ezeokekeobianuju@gmail.com>
2022-09-15 22:15:23 +0100

    Window.alert

--- First script.js
+++ First script.js
@@ -1,1 +1,3 @@
-console.log();
+console.log("I like pizza, it is a great way to start my day " )
+console.log("My first JavaScript. Hello world!");
+window.alert("For security reasons, please accept cookies before proceeding. ")
