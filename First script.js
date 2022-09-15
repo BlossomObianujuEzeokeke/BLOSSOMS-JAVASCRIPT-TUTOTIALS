05be378921b4fd84948d8dab8399f9f5db90ffa7
BlossomEze <ezeokekeobianuju@gmail.com>
2022-09-16 00:56:42 +0100

    Data types

--- First script.js
+++ First script.js
@@ -1,21 +1,29 @@
  console.log("I like pizza, it is a great way to start my day " )
 console.log("My first JavaScript. Hello world!");
-window.alert("For security reasons, please accept cookies before proceeding.")
+
+// window.alert("For security reasons, please accept cookies before proceeding.")
 
 //-------- HOW TO DECLARE AND ASSIGN  A VARIABLE ------//
 // let age; (this is best for user input, only declare, dont assign)
 // age = 21;
 let age = 21; //21 is a number data type
+// age = age + 1;
+age++;
+
 let firstName = "Blossom"; //string data type
-let secondName = 'BlossomEze';
-// let secondName = 'BlossomEze ' + " Hadiza " + "Simi";
-var student = "False"; //boolean true or false 
+let secondName = 'Ezeokeke';
+// let secondName = 'Ezeokeke ' + " Hadiza " + "Simi";
+var student = "True"; //boolean true or false 
 
 console.log(age);
 console.log(firstName);
 console.log(secondName);
 console.log(student);
 
-
+//--- MAKING THESE INPUTS INTERACTIVE ---//
+console.log("Hello", firstName);
+console.log("Your surname is", secondName);
+console.log("You are", age, "years old");
+console.log("Enrolled:", student);
 
 
