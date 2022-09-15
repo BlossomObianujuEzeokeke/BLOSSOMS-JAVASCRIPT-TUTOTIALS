4bbcf77600bfb900d230ba573bf9360b0a03cb0a
BlossomEze <ezeokekeobianuju@gmail.com>
2022-09-15 23:22:31 +0100

    Data types

--- First script.js
+++ First script.js
@@ -1,3 +1,21 @@
-console.log("I like pizza, it is a great way to start my day " )
+ console.log("I like pizza, it is a great way to start my day " )
 console.log("My first JavaScript. Hello world!");
-window.alert("For security reasons, please accept cookies before proceeding. ")
+window.alert("For security reasons, please accept cookies before proceeding.")
+
+//-------- HOW TO DECLARE AND ASSIGN  A VARIABLE ------//
+// let age; (this is best for user input, only declare, dont assign)
+// age = 21;
+let age = 21; //21 is a number data type
+let firstName = "Blossom"; //string data type
+let secondName = 'BlossomEze';
+// let secondName = 'BlossomEze ' + " Hadiza " + "Simi";
+var student = "False"; //boolean true or false 
+
+console.log(age);
+console.log(firstName);
+console.log(secondName);
+console.log(student);
+
+
+
+
