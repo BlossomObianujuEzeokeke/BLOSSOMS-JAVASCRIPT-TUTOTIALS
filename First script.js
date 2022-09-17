3fececbde7253448d584939b256571381612fe1d
BlossomEze <ezeokekeobianuju@gmail.com>
2022-09-17 11:09:03 +0100

    DOM

--- First script.js
+++ First script.js
@@ -1,37 +1,56 @@
- console.log("I like pizza, it is a great way to start my day " )
-console.log("My first JavaScript. Hello world!");
+   //  console.log("I like pizza, it is a great way to start my day " )
+   // console.log("My first JavaScript. Hello world!");
 
-// window.alert("For security reasons, please accept cookies before proceeding.")
+   // window.alert("For security reasons, please accept cookies before proceeding.")
 
-//-------- HOW TO DECLARE AND ASSIGN  A VARIABLE ------//
-// let age; (this is best for user input, only declare, dont assign)
-// age = 21;
-let age = 21; //21 is a number data type
-// age = age + 1;
-age++;
+   //-------- HOW TO DECLARE AND ASSIGN  A VARIABLE ------//
+   // let age; (this is best for user input, only declare, don't assign)
+   // age = 21;
+   // let age = 21; //21 is a number data type
+   // age = age + 1;
+   // age++;
 
-let firstName = "Blossom"; //string data type
-let secondName = 'Ezeokeke';
-// let secondName = 'Ezeokeke ' + " Hadiza " + "Simi";
-var student = "True"; //boolean true or false 
+   // let firstName = "Blossom"; //string data type
+   // let secondName = 'Ezeokeke';
+   // let secondName = 'Ezeokeke ' + " Hadiza " + "Simi";
+   // var student = "True"; //boolean true or false 
 
-console.log(age);
-console.log(firstName);
-console.log(secondName);
-console.log(student);
+   // console.log(age);
+   // console.log(firstName);
+   // console.log(secondName);
+   // console.log(student);
 
-//--- MAKING THESE INPUTS INTERACTIVE ---//
-console.log("Hello", firstName);
-console.log("Your surname is", secondName);
-console.log("You are", age, "years old");
-console.log("Enrolled:", student);
+   //--- MAKING THESE INPUTS INTERACTIVE ---//
+   // console.log("Hello", firstName);
+   // console.log("Your surname is", secondName);
+   // console.log("You are", age, "years old");
+   // console.log("Enrolled:", student);
 
-document.getElementById("p1").innerHTML = "Hello " + firstName + "<hr> " + firstName + "<hr>";
-document.getElementById("p2").innerHTML = "<h1>" + "Your second name is " + secondName + "&nbsp" + firstName + "</h1>";
-document.getElementById("p3").innerHTML = "You have just turned " + age + "<img src="./first image.jpg" alt="" > ";
+   // document.getElementById("p1").innerHTML = "Hello " + firstName + "<hr> " + firstName + "<hr>";
+   // document.getElementById("p2").innerHTML = "<h1>" + "Your second name is " + secondName + "&nbsp" + firstName + "</h1>";
+   // document.getElementById("p3").innerHTML = "You have just turned " + age + "<img src="./first image.jpg" alt="" > ";
 
- 
+   //---------- ARITHMETIC EXPRESSIONS -----------//
+   let students = 20;
+
+   //---- USING OPERATORS(+)-----//
+   //  students = students + 1; 
+   //  students += 1;
+   // students++;
+
+   //---- USING OPERATORS(-)-----//
+   students = students - 1; 
+   students -= 1;
+   // students--;
+
+   //---- USING OPERATORS(*)-----//
+   // students = students * 2; 
+   //  students *= 2;
+
+   //---- USING OPERATORS(%)-----//
+   // students = students % 2; 
+   //  students %= 2;
 
 
-
+   console.log("There are", students, "in my class");
 
