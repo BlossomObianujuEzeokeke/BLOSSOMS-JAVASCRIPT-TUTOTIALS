e1fa4656e67221d84aaf06e0ea9a9345a5c9e583
BlossomEze <ezeokekeobianuju@gmail.com>
2022-09-17 12:30:11 +0100

    DOM

--- First script.js
+++ First script.js
@@ -1,7 +1,7 @@
    //  console.log("I like pizza, it is a great way to start my day " )
    // console.log("My first JavaScript. Hello world!");
 
-   window.alert("For security reasons, please accept cookies before proceeding.")
+   // window.alert("For security reasons, please accept cookies before proceeding.")
 
    //-------- HOW TO DECLARE AND ASSIGN  A VARIABLE ------//
    // let age; (this is best for user input, only declare, don't assign)
