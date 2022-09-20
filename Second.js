784c40909570f6f851af1ed05059d0c91fd0f880
BlossomEze <ezeokekeobianuju@gmail.com>
2022-09-20 12:59:55 +0100

    Typeof

--- Second.js
+++ Second.js
@@ -1,5 +1,5 @@
-let age = 25;
+let age = 25; 
 let message = "I am";
-document.getElementById("demo").innerHTML = message + age + "years old"
-typeof message
-console.log(message);
+document.body.innerHTML = message + " " + age + " " + "years old"
+console.log(age);
+console.log(typeof message);
